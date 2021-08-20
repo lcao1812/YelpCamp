1ec53435993e546cc3e1accaf2670211f0e019f9
ldc1812 <locdcao@gmail.com>
2021-08-20 12:19:39 -0400

    update

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+
+const Campground = require('./models/campground')
+
+mongoose.connect('mongodb://localhost:27017/YelpCamp', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error: '));
+db.once('open', () => {
+    console.log('Database connected');
+})
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+/* HOME -> "/"  */
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Yelp Camp' });
+})
+
+/* INDEX -> "/campgrounds" */
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { title: 'Index', campgrounds });
+})
+
+/* CREATE */
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new', { title: 'New campground' });
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const { title, location } = req.body.campground;
+    const campground = new Campground({ title: title, location: location });
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+/* SHOW -> "/campgrounds/<id>" */
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { title: `${campground.title}`, campground });
+})
+
+/* EDIT -> "/campgrounds/<id>/edit" */
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { title: `${campground.title}`, campground });
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+
+    const campground = await Campground.findByIdAndUpdate(req.params.id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+/* DELETE -> "/campgrounds/<id>" */
+app.delete('/campgrounds/:id', async (req, res) => {
+    await Campground.findByIdAndDelete(req.params.id);
+    res.redirect('/campgrounds')
+})
+
+app.listen(3000, function () {
+    console.log("SERVING ON PORT 3000");
+})
